24166c981b3429fa10a2e3dab20a7c895486659b
Xalizar6 <dave.rukamp@gmail.com>
2026-04-13 00:01:47 -0500

    add RoomConstruction logic to place containers near the energy sources

--- src/management/roomManager.ts
+++ src/management/roomManager.ts
@@ -1,11 +1,20 @@
+import {
+  CONSTRUCTION_PLAN_INTERVAL,
+  runRoomConstruction,
+} from "./roomConstruction";
+
 export const LOG_MODULE = "roomManager" as const;
 
 export const runRoomManagement = (): void => {
+  const runConstructionPlan = Game.time % CONSTRUCTION_PLAN_INTERVAL === 0;
   for (const roomName in Game.rooms) {
     const room = Game.rooms[roomName];
     if (!room) {
       continue;
     }
     room.memory.lastManagedTick = Game.time;
+    if (runConstructionPlan) {
+      runRoomConstruction(room);
+    }
   }
 };
